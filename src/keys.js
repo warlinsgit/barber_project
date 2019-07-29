95a52045cf7a13b20c09bd2317dfac99b55f2927
warlinsgit <warlins25@gmail.com>
2019-07-30 00:02:45 +0100

    3.4

--- src/keys.js
+++ src/keys.js
@@ -14,18 +14,7 @@ module.exports = {
 /* git  git remote set-url origin https*/
 
 
-/*
-module.exports = {
-  database: {
-    host: 'db4free.net',
-    port: '3306',
-    user: 'warlinsvida',
-    password: 'varlei2020',
-    dateStrings:true,
-    database: 'barbershop'
-  }
-}
-*/
+
 
 
 
