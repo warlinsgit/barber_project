de5bc78bb42366bc01c934ad2b9a359b24a53e3a
warlinsgit <33092160+warlinsgit@users.noreply.github.com>
2019-07-30 00:26:11 +0100

    Update authentication.js

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -263,7 +263,7 @@ const amigo = pool.query("UPDATE users_b SET coded = ? WHERE email = ?;", [vida,
   var transporter=nodemailer.createTransport( {
 
     service: 'gmail', auth: {
-     user: 'matutinolife@gmail.com', pass: 'matutino3030'
+     user: 'matutinolife@gmail.com', pass: ''
     }
   }
 );
