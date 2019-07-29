71920c163ce3d6af381d0a4c233c7acf7564849d
warlinsgit <warlins25@gmail.com>
2019-07-29 23:25:32 +0100

    2.2

--- src/index.js
+++ src/index.js
@@ -93,8 +93,8 @@ app.use('/services', require('./routes/services'));
 
 // Public
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.normalize(path.join(__dirname, './public')), { maxAge: 86400000 }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
