51f4a9eb6bab52faff8993cca63719b90e2f89ef
warlinsgit <warlins25@gmail.com>
2019-08-03 01:35:20 +0100

    fixing i

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -225,6 +225,10 @@ router.post('/confirm_booking/', async(req, res)=>{
 
 
  var transporter=nodemailer.createTransport( {
+
+   host: 'smtp.gmail.com',
+   port: 465,
+   secure: true,
    service: 'gmail', auth: {
      user: 'matutinolife@gmail.com', pass: 'matutino3030'
    }
